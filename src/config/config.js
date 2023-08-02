cfb8c11afdf01fa6808f341af80cfe3198d782af
dhuyartmaker <duchuy2411itd@gmail.com>
2023-08-02 23:55:25 +0700

    config model + db

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,12 @@
-const overwriteDevConfig = require('./dev.config.json');
 const overwriteDefaultConfig = require('./config.json');
 
 const configDefault = {
 }
 const env = process.env.NODE_ENV || "pro";
-console.log("===env===", env);
-
+console.log("=env=", env)
 module.exports = {
     config: {
         ...configDefault,
-        ...(env === "dev" ? overwriteDevConfig : overwriteDefaultConfig),
+        ...overwriteDefaultConfig,
     }
 }
